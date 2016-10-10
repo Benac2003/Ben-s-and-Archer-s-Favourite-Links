82099e1a84358a29f36d73bd9e365e7594f7ed4a
Benac2003 <ben.weber@amnet.net.au>
2016-10-10 20:15:59 +0800

    Update Request.js

--- Request.js
+++ Request.js
@@ -1,9 +1,20 @@
  //choices
  var choice = prompt("What do you like(anime, Computer Games... choices still in progress");
- var type = null;
- var request = function(choice,type) {
-  if (choice == anime) {
+//types 
+var type = null;
+//function requst 
+var request = function(choice,type) {
+ 
+ if (choice == "anime" && "Computer Games") {
+  Confirm("Not Invalid, Ask again.");
+ //anime 
+ if (choice == "anime") {
   confirm("Free Anime on this Website, http://kissanime.to/");
   }
+ //games
+  if (choice == "Computer Games") {
+   confirm("Free old Games on this Website, http://www.emuparadise.me/ and Download Program at ...(Website Here)");
+  }
+  
  }
 request(choice,type);
