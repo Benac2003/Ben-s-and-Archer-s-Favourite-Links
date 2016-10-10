161ef01a2436fbfad752403e8248af5e2f7874cb
Benac2003 <ben.weber@amnet.net.au>
2016-10-10 19:45:14 +0800

    Update Request.js

--- Request.js
+++ Request.js
@@ -1,8 +1,9 @@
  //choices
- var choice = prompt("What do you like(anime... choices still in progress");
+ var choice = prompt("What do you like(anime, Computer Games... choices still in progress");
  var type = null;
  var request = function(choice,type) {
   if (choice == anime) {
-  confirm("Free Anime on this Website, http://kissanime.to/);
+  confirm("Free Anime on this Website, http://kissanime.to/");
   }
  }
+request(choice,type);
