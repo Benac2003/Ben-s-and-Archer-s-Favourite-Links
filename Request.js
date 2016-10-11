6f9de9c5dd5fd8d4f200f397d6bef0e301232706
Benac2003 <ben.weber@amnet.net.au>
2016-10-11 20:22:25 +0800

    Update Request.js

--- Request.js
+++ Request.js
@@ -1,12 +1,12 @@
- //choices
+while(done) {
+var time = 3;
+var done = true;
+//choices
  var choice = prompt("What do you like(anime, Computer Games... choices still in progress");
 //types 
 var type = null;
 //function requst 
-var request = function(choice,type) {
- 
- if (choice == "anime" && "Computer Games") {
-  Confirm("Not Invalid, Ask again.");
+var request = function(choice, time) {
  //anime 
  if (choice == "anime") {
   confirm("Free Anime on this Website, http://kissanime.to/");
@@ -15,6 +15,11 @@ var request = function(choice,type) {
   if (choice == "Computer Games") {
    confirm("Free old Games on this Website, http://www.emuparadise.me/ and Download Program at ...(Website Here)");
   }
-  
- }
-request(choice,type);
+  if (time === 3) {
+   Confirm("All of the choices has run out, if you want to do this again, please re-run.");
+    done == false;
+  }
+  time++;
+  }
+request(choice, time);
+}
